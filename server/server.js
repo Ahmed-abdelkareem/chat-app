71958eb579f321fb28c473fa61696c1ddc4166d8
Ahmed-abdelkareem <32497527+Ahmed-abdelkareem@users.noreply.github.com>
2018-01-20 12:10:07 +0200

    add webSockets

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
 const path = require('path');
-const publicPath = path.join(__dirname, '../public');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
+const publicPath = path.join(__dirname, '../public');
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+io.on('connection',(socket)=>{
+console.log('new user connected');
+socket.on('disconnect',()=>{
+console.log('cleint disconnected');
+})
+})
+
+
+
+
 app.use(express.static(publicPath));
 const port = process.env.PORT || 8080;
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`server is running on port ${port}`)
-})
+});
